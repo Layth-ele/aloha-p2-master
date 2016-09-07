404de40be9070af6bd6e0d99f6d55d905ccf34d1
Layth-ele <layth.ele@gmail.com>
2016-09-07 11:10:06 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('.product-slider').flickity({
   wrapAround: true
 });
 
-$('form.singUp').on("submit",function(event){
+$('form').on("submit",function(event){
   event.preventDefault();
   var userEmail = $('input [type="email"]').val();
   if (userEmail){
@@ -19,3 +19,38 @@ $('form.singUp').on("submit",function(event){
   }
 
 });
+$(function(){
+$('a[href*="#"]:not([href="#"])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+   && location.hostname == this.hostname) {
+       var $target = $(this.hash);
+       $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+       if ($target.length) {
+           var targetOffset = $target.offset().top;
+           $('html,body').animate({scrollTop: targetOffset - 100}, 1000);
+           return false;
+       }
+   }
+ });
+});
+
+// $('form').on('submit', function(event) {
+//
+//       var $self = $(this),
+//           $pulse = $self.siblings('.pulse-overlay'),
+//           offsetTop = $self.offset().top, // button offset
+//           offsetLeft = $self.offset().left, // button offset
+//           clickedX = event.pageX, // x position of click to doc
+//           clickedY = event.pageY, // y position of click to doc
+//           centerX = ( clickedX - offsetLeft ),
+//           centerY = ( clickedY - offsetTop );
+//
+//       $pulse.addClass('radial-pulse').show();
+//       $pulse.css({top: centerY, left: centerX});
+//
+//       setTimeout(function() {
+//         $pulse.removeClass('radial-pulse');
+//       }, 1000);
+//
+//   });
+//   });
